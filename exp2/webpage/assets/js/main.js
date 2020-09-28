7374dbf7066dd7433fc872480156da04b0a5fd61
Sree Lakshmi <tatikondasreelakshmi@gmail.com>
2020-09-29 00:40:09 +0530

    29-09-2020

--- exp2/webpage/assets/js/main.js
+++ exp2/webpage/assets/js/main.js
@@ -1,8 +1,4 @@
-/*
-	Industrious by TEMPLATED
-	templated.co @templatedco
-	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
-*/
+
 (function ($) {
 
 	var copyRightHTML = '<div class="copyright">&copy; Tatikonda Sree Lakshmi, Research Engineer, VLabs IITKGP.</div>';
